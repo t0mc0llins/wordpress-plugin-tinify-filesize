137e73efd2dfae4f2c561830e863ca6d65b6b99d
Tom Collins <tomcollins@voormedia.com>
2022-07-14 16:36:14 +0200

    added one time metadata fix script (will not run currently)

---
+++ one-time-update-script.php
@@ -0,0 +1,13 @@
+<?php
+$args = array('post_type'=>'attachment','numberposts'=>null,'post_status'=>null);
+$attachments = get_posts($args);
+if($attachments){
+    foreach($attachments as $attachment){
+			$wp_data = wp_get_attachment_metadata( $attachment->ID);
+			$tiny_size = wp_filesize( get_attached_file($attachment->ID));
+			if ($tiny_size  != $wp_data['filesize']) {
+				$wp_data['filesize'] = $tiny_size;
+				wp_update_attachment_metadata( $attachment->ID, $wp_data );
+			}
+		}
+}
